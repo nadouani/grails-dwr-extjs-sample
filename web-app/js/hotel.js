d48e521b54c1f928b1d5de1aaa5d10d4d267bc62
Nabil Adouani <nabil.adouani@gmail.com>
2010-09-02 00:26:28 +0200

    Add grid editors, toolbar and bottombar, autosave and batch update

--- web-app/js/hotel.js
+++ web-app/js/hotel.js
@@ -1,3 +1,134 @@
+Ext.ns('App', 'App.hotel');
+
+App.hotel.Grid = Ext.extend(Ext.grid.EditorGridPanel, {
+	renderTo: 'hotel-grid',
+	iconCls: 'silk-hotel',
+    frame: true,
+    title: 'Hotels',
+    height: 300,
+    width: 800,
+    style: 'margin-top: 10px',
+
+    initComponent : function() {
+
+        // typical viewConfig
+        this.viewConfig = {
+            forceFit: true
+        };
+
+        // relay the Store's CRUD events into this grid so these events can be conveniently listened-to in our application-code.
+        //this.relayEvents(this.store, ['destroy', 'save', 'update']);
+
+        // build toolbars and buttons.
+        this.cm = this.buildColumnModel();
+        this.tbar = this.buildTopToolbar();
+        this.bbar = this.buildBottomToolbar();
+
+        // super
+        App.hotel.Grid.superclass.initComponent.call(this);
+    },
+    
+    buildColumnModel : function(){
+    	var defaultEditor = new Ext.form.TextField({
+            allowBlank: false
+    	});
+    	
+    	return new Ext.grid.ColumnModel({
+	    	defaults: {
+	          sortable: true // columns are not sortable by default           
+	      	},
+	      	columns: [
+	      	    { header: '#', readOnly: true, dataIndex: "id", width: 40, hidden: false},
+	      	    { header: 'Name', dataIndex: "name", width:150, editor: defaultEditor},
+	      	    { header: 'Website', dataIndex: "website", width:150, editor: defaultEditor},
+				{ header: 'Phone', dataIndex: "phoneNumber", editor: defaultEditor},
+				{ header: 'City', dataIndex: "city", editor: defaultEditor},
+				{ header: 'Country', dataIndex: "country", editor: defaultEditor},
+				{ header: '# Stars', dataIndex: "stars", width:50, editor: defaultEditor},
+				{ header: '# Rooms', dataIndex: "rooms", width:60, editor: defaultEditor},
+				{ header: '# Beds', dataIndex: "beds", width:50, editor: defaultEditor},
+	        ]
+	    })
+    },
+
+    buildTopToolbar : function() {
+        return [{
+            text: 'Add',
+            iconCls: 'silk-add',
+            handler: this.onAdd,
+            scope: this
+        }, '-', {
+            text: 'Delete',
+            iconCls: 'silk-delete',
+            handler: this.onDelete,
+            scope: this
+        }, '-'];
+    },
+
+    buildBottomToolbar : function() {
+        return [{
+            text: 'autoSave',            
+            iconCls: 'silk-auto-save',
+            enableToggle: true,
+            tooltip: 'When enabled, Store will execute Ajax requests as soon as a Record becomes dirty.',
+            toggleHandler: function(btn, pressed) {
+                this.store.autoSave = pressed;
+            },
+            scope: this
+        }, '-', {
+            text: 'batch',
+            iconCls: 'silk-batch',
+            enableToggle: true,
+            tooltip: 'When enabled, Store will batch all records for each type of CRUD verb into a single Ajax request.',
+            toggleHandler: function(btn, pressed) {
+                this.store.batch = pressed;
+            },
+            scope: this
+        }, '-', {
+            text: 'writeAllFields',
+            iconCls: 'silk-write',
+            enableToggle: true,
+            tooltip: 'When enabled, Writer will write *all* fields to the server -- not just those that changed.',
+            toggleHandler: function(btn, pressed) {
+                this.store.writer.writeAllFields = pressed;
+            },
+            scope: this
+        }, '->', {
+            text: 'Save',
+            iconCls: 'silk-save',
+            handler: this.onSave,
+            scope: this
+        }];
+    },
+
+    onSave : function(btn, ev) {
+        this.store.save();
+    },
+
+    onAdd : function(btn, ev) {
+    	/*
+        var u = new this.store.recordType({
+            first : '',
+            last: '',
+            email : ''
+        });
+        this.stopEditing();
+        this.store.insert(0, u);
+        this.startEditing(0, 1);
+        */
+    },
+
+    onDelete : function(btn, ev) {
+        var index = this.getSelectionModel().getSelectedCell();
+        if (!index) {
+            return false;
+        }
+        var rec = this.store.getAt(index[0]);
+        this.store.remove(rec);
+    }
+});
+
+
 Ext.onReady(function() {
     Ext.QuickTips.init();
 
@@ -45,48 +176,10 @@ Ext.onReady(function() {
     });
     
     hotelStore.load();
-
-    var hotelGrid = new Ext.grid.GridPanel({
-    	renderTo: 'hotel-grid',        
-        store: hotelStore,
-        frame: true,
-        title: 'Hotels',
-        height: 300,
-        width: 800,
-        style: 'margin-top: 10px',
-        viewConfig : {
-            forceFit: true
-        },
-        // build toolbars and buttons.
-        cm : new Ext.grid.ColumnModel({
-	    	defaults: {
-	          sortable: true // columns are not sortable by default           
-	      	},
-	      	columns: [
-	      	    { header: '#', readOnly: true, dataIndex: "id", width: 40, hidden: false},
-	      	    { header: 'Name', dataIndex: "name", width:150},
-	      	    { header: 'Website', dataIndex: "website", width:150},
-				{ header: 'Phone', dataIndex: "phoneNumber"},
-				{ header: 'City', dataIndex: "city"},
-				{ header: 'Country', dataIndex: "country"},
-				{ header: '# Stars', dataIndex: "stars", width:50},
-				{ header: '# Rooms', dataIndex: "rooms", width:60},
-				{ header: '# Beds', dataIndex: "beds", width:50},
-	        ]
-	    })
+    
+    
+    var hotelGrid = new App.hotel.Grid({
+        renderTo: 'hotel-grid',
+        store: hotelStore
     });
-
-    
-    //hotelGrid.show();
-    
-    
-    /*
-    var hotelPanel = new Ext.Panel({
-        title: 'My Panel',
-        collapsible:true,
-        renderTo: 'hotel-form',
-        width:400,
-        html: 'Hello'
-    });
-    */
 });
